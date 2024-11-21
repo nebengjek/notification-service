8913c2711a96a896312127452b2a5eea419f2dc0
Farid Tri Wicaksono <farid.wicak@gmail.com>
2024-11-21 18:32:08 +0700

    fix: extend ttl redis tripplan

--- bin/modules/notification/usecases/command_usecase.go
+++ bin/modules/notification/usecases/command_usecase.go
@@ -2,12 +2,15 @@ package usecases
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	driver "notification-service/bin/modules/notification"
 	"notification-service/bin/modules/notification/models"
 	httpError "notification-service/bin/pkg/http-error"
 	kafkaPkgConfluent "notification-service/bin/pkg/kafka/confluent"
+	"notification-service/bin/pkg/log"
+	"notification-service/bin/pkg/utils"
 
 	"github.com/redis/go-redis/v9"
 )
@@ -46,6 +49,16 @@ func (c *commandUsecase) SendNotification(ctx context.Context, payload models.Tr
 		errObj := httpError.BadRequest("Profile Driver not completed")
 		return errObj
 	}
+
+	key := fmt.Sprintf("USER:ROUTE:%s", payload.PassengerID)
+	newTTL := time.Duration(60) * time.Minute
+	redisErr := c.redisClient.Expire(ctx, key, newTTL).Err()
+	if redisErr != nil {
+		log.GetLogger().Error("command_usecase", fmt.Sprintf("Error updating TTL in Redis: %v", redisErr), "UpdateTTL", utils.ConvertString(redisErr))
+		errObj := httpError.InternalServerError(fmt.Sprintf("Error updating TTL: %v", redisErr))
+		return errObj
+	}
+
 	driver, _ := driverInfo.Data.(models.User)
 	notification := models.Notification{
 		RecipientID: payload.PassengerID,
